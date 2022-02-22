8aa1057af494fa1d19457cbf2bcb84de5276b71c
ddevineFIU <ddevi015@fiu.edu>
2022-02-21 21:12:57 -0500

    test

---
+++ Favorite/src/module-info.java
@@ -0,0 +1,9 @@
+
+
+public class Favorite{
+	
+	public static void main(String[] args) {
+		boolean like = true;
+		System.out.println(like);
+		}
+}
---
+++ test/src/test.java
@@ -0,0 +1,12 @@
+public class test {
+	
+	public static void main(String[] args) {
+
+		boolean like = true;
+		
+		if(true) {
+			System.out.println("You have added this to your favorite list.");
+		}
+		
+	}	
+}
